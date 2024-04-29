61463a4a06a78bf5017d99e114e9bdf24e59842c
palkon-code <vbwpro@gmail.com>
2024-04-29 15:57:06 +0700

    Search Method

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserserviceService } from '../userservice.service';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -10,22 +11,37 @@ import { Router } from '@angular/router';
 export class LoginPage {
   constructor(
     private userService: UserserviceService,
-    private router: Router
+    private router: Router,
+    private toastController: ToastController
   ) {
     console.log(userService.users)
   }
 
   username = '';
   password = '';
-
+  found=false;
   Login() {
+    this.found=false;
     for (const user of this.userService.users) {
       if (user.username === this.username && user.password === this.password) {
+        this.found=true;
         this.userService.userLogin=user;
         this.router.navigate(["tabs/home"]);
         break;
       }
     }
+    if(!this.found){
+      this.presentToast("Gagal Login");
+    }
+
+  }
+  async presentToast(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 1500,
+      position: 'bottom',
+    });
+    await toast.present();
   }
   ToRegister(){
     this.router.navigate(['register']);
